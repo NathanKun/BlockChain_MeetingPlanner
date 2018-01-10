69c492a835d0294b7c8b5ed9158ee74497a51a8f
moucharaf <moucharaf.ibrahim@gmail.com>
2018-01-10 11:25:49 +0100

    Add struct meeting and function fro create and search a meeting

--- contracts/MeetingPlanner.sol
+++ contracts/MeetingPlanner.sol
@@ -2,7 +2,43 @@ pragma solidity ^0.4.2;
 
 contract MeetingPlanner {
 
-	function MeetingPlanner() {
-		
+	struct Meeting {
+		uint id;
+		bool required;
+		string manager;
+		string description;
+		string lieu;
+		uint date;
 	}
+
+    string name;
+
+	function MeetingPlanner() {
+
+	}
+
+	// Create a meeting with a description argument
+    function CreateMeeting(string description) public {
+		Meeting memory newMeeting =  Meeting({
+			id: 1,
+			manager: name,
+			description: description,
+		    required: false ,
+		    lieu: 'rue',
+		    date: 8
+		});
+    }
+
+	//Search a meeting with some id argument
+	function SearchMeeting(uint id ) public returns (bool exist){
+    uint j;
+    for( j=0 ; j < Meeting_List[].length() ; j++){
+        if(Meeting[j].id == id ){
+            return true;
+        }
+        else {
+            return false;
+        }
+        }
+    }
 }
