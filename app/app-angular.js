7669857aab9cf4eaacb3b9d96efa58606faea2fe
Yuzhou <syzadele@outlook.com>
2018-01-11 00:02:01 +0100

    Add login form

--- app/app-angular.js
+++ app/app-angular.js
@@ -1,5 +1,5 @@
 var myApp = angular.module('app', []);
 
 myApp.controller('helloworldController', function ($scope, $rootScope) {
-    $scope.title = "Hello World!";
+    $scope.title = "Welcome to meeting planner!";
 });
