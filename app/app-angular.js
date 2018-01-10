116cbcd42fcf55891bbd81f36363aa100a09690d
HE Junyang <nathanhejunyang@gmail.com>
2018-01-11 00:21:23 +0100

    Added js function to get desciption and creat meeting

--- app/app-angular.js
+++ app/app-angular.js
@@ -1,5 +1,27 @@
 var myApp = angular.module('app', []);
 
+myApp.service('MeetingIndexService', function($rootScope, $http) {
+	
+	var meetingIndexService = {};
+    deployedContract = MetaCoin.deployed();
+	
+	meetingIndexService.createContract = function(description) {
+		return deployedContract.CreateMeeting(description);
+	}
+	
+	return meetingIndexService;
+	
+}
+	
+myApp.controller('meetingIndexController', function (MeetingIndexService, $scope, $rootScope) {
+
+	$scope.createMeeting = function() {
+		MeetingIndexService.createContract($scope.description);
+	}
+	
+});
+
+
 myApp.controller('helloworldController', function ($scope, $rootScope) {
     $scope.title = "Hello World!";
 });
