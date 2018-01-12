b410ae48fbf11758d31543441ab64e64adfd7f3b
Yuzhou <syzadele@outlook.com>
2018-01-12 11:53:41 +0100

    Little modification in js file

--- app/app-angular.js
+++ app/app-angular.js
@@ -1,22 +1,22 @@
-// declare a new Angular module 
+// declare a new Angular module
 var myApp = angular.module('app', []);
 
 // declare angular service MeetingIndexService
 myApp.service('MeetingIndexService', function($rootScope, $http) {
-	
+
 	// empty service object
 	var meetingIndexService = {};
 	// MeetingPlanner contract
 	deployedContract = MeetingPlanner.deployed();
-	
+
 	// add createContract function to service
 	meetingIndexService.createContract = function(description) {
 		return deployedContract.CreateMeeting(description);
 	}
-	
+
 	// return completed service
 	return meetingIndexService;
-	
+
 }
 
 // controller for MeetingIndex page
@@ -27,10 +27,13 @@ myApp.controller('meetingIndexController', function (MeetingIndexService, $scope
 		// call createContract() in MeetingIndexService service and pass 'description' from page to it
 		MeetingIndexService.createContract($scope.description);
 	}
-	
+
 });
 
 
-myApp.controller('helloworldController', function ($scope, $rootScope) {
-    $scope.title = "Welcome to meeting planner!";
+myApp.controller('LoinIndexController', function ($scope, $rootScope) {
+    $scope.title = "Welcome to the meeting planner!";
+		$scope.verifiyLogin = function(){
+			
+		}
 });
